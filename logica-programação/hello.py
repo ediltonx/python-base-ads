2d16ede76d6990bb56682c650385fce44ae5ae0c
Edilton Jr <ediltonx@hotmail.com>
2024-08-28 10:29:34 -0300

    Adicionado o Hello World Pythonicz

---
+++ logica-programação/hello.py
@@ -0,0 +1,26 @@
+from rich import print  # type: ignore
+
+# ESCREVER FRASES EM PYTHON
+
+# modo mais simples
+
+print ("Hello Pythoners")
+
+# escrever usando .format
+
+print ("{} {} usando {}".format("Hello" , "Pythoners", "Format"))
+
+# Modo herdado da linguagem C referenciando endereço de memoria
+
+print ("%s %s" % ("Hello", "Pythoners  com E comercial"))
+
+# Formatação de Strings Literal
+a = "Hello"
+aa = "Pythoners"
+aaa = "Com fstring"
+
+print (f"{a} {aa} {aaa}")
+
+# usando a biblioteca rich para mudar o estilo do texto impresso
+
+print("[bold blue]Hello Pythoners[/bold blue] [italic green]totalmente[/italic green] [red]estilizado[/red]")
