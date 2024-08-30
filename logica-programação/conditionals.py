b7c5feae2f6079af30b831915f0d16ab7260dd3c
Tyr McDohl <gregminster@localhost.localdomain>
2024-08-30 20:12:05 -0300

    Iniciando condicionais

---
+++ logica-programação/conditionals.py
@@ -0,0 +1,23 @@
+# CONDICIONAIS COM PYTHON
+
+
+
+
+# O comando se - então - senão  é feito de forma identada para separar o fluxo de instrução do resto do programa
+
+x = 5
+
+if x > 0 :
+    print(x, " é maior que zero.")
+else: print (x, " é menor que zero.")
+
+# Caso tenha várias condicionais utilize elif
+
+y = 8
+
+if y > 0: 
+    print( y, " é maior que zero.")
+elif y < 0:
+    print ( y, " é menor que zero.")
+else :
+    print (y," é igual a zero.")
